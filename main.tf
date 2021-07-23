d1cee24a7db5295e50e09f436b232cae9d32b409
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-23 18:24:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
 
 locals {
   vcn = data.oci_core_vcn.vcn
+  public_addr = length(var.public_addr_id) > 0 ? var.public_addr_id : oci_core_public_ip.this[0].id
 }
 
 data "oci_core_vcn" "vcn" {
@@ -21,19 +22,12 @@ data "oci_core_vcn" "vcn" {
 resource "oci_core_subnet" "this" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
-  prohibit_public_ip_on_vnic = false
+  prohibit_public_ip_on_vnic = true
 
   cidr_block = var.cidr
   route_table_id = oci_core_route_table.this.id
   display_name = var.name
   dns_label = var.name
-}
-
-resource "oci_core_internet_gateway" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-  
-  display_name = var.name
 }
 
 resource "oci_core_route_table" "test_route_table" {
@@ -50,3 +44,21 @@ resource "oci_core_route_table" "test_route_table" {
     destination = "0.0.0.0/0"
   }
 }
+
+resource "oci_core_public_ip" "this" {
+  count = length(var.public_addr_id) > 0 ? 1 : 0
+  
+  compartment_id = local.vcn.compartment_id
+  lifetime = "RESERVED"
+
+  display_name = var.public_ip_display_name
+}
+
+resource "oci_core_nat_gateway" "this" {
+  compartment_id = local.vcn.compartment_id
+  vcn_id = local.vcn.id
+
+  display_name = var.name
+  public_ip_id = local.public_addr
+}
+
