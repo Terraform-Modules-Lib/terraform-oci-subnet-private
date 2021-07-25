295da7396d0cce6aadbd04df45b6db77ffb1dde7
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-25 14:59:07 +0200

    Update oci_gw.tf

--- oci_gw.tf
+++ oci_gw.tf
@@ -1,2 +1,20 @@
 data "oci_core_services" "oci_services" {
+  
+}
+
+resource "oci_core_service_gateway" "oci_gw" {
+  compartment_id = local.subnet.compartment_id
+  vcn_id = ocal.subnet.vcn_id
+
+  display_name = local.name
+
+  dynamic "services" {
+    for_each = { for service in data.oci_core_services.oci_services: 
+      service.id => service
+    }
+
+    contenct {
+      service_id = services.value.id
+    }
+  }
 }
