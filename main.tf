2c8eabf138592a46965c5b096be435a8d77354d6
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-25 10:57:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,53 +11,22 @@ terraform {
 }
 
 locals {
-  vcn = data.oci_core_vcn.vcn
-  public_addr = length(var.public_addr_id) > 0 ? var.public_addr_id : oci_core_public_ip.this[0].id
-}
-
-data "oci_core_vcn" "vcn" {
+  name = var.name
+  cidr = var.cidr
+  
   vcn_id = var.vcn_id
-}
-
-resource "oci_core_subnet" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-  prohibit_public_ip_on_vnic = true
-
-  cidr_block = var.cidr
-  route_table_id = oci_core_route_table.this.id
-  display_name = var.name
-  dns_label = var.name
-}
-
-resource "oci_core_route_table" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-
-  display_name = var.name
+  public_addr_id = var.public_addr_id
   
-  route_rules = [{
-    network_entity_id = oci_core_nat_gateway.this.id
-    
-    description = "Internet route"
-    destination_type = "CIDR_BLOCK"
-    destination = "0.0.0.0/0"
-  }]
+  vcn = data.oci_core_vcn.vcn
+  subnet = module.subnet.subnet
+  #public_addr = length(var.public_addr_id) > 0 ? var.public_addr_id : oci_core_public_ip.this[0].id
 }
 
-resource "oci_core_public_ip" "this" {
-  count = length(var.public_addr_id) > 0 ? 1 : 0
+module "subnet" {
+  source  = "Terraform-Modules-Lib/subnet/oci"
   
-  compartment_id = local.vcn.compartment_id
-  lifetime = "RESERVED"
-
-  display_name = var.public_ip_display_name
-}
-
-resource "oci_core_nat_gateway" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-
-  display_name = var.name
-  public_ip_id = local.public_addr
+  name = local.name
+  cidr = local.cidr
+  vcn_id = local.vcn_id
+  public = false
 }
