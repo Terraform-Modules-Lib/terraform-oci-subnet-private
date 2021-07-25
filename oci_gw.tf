dd0cf47dadd02c25fda97d70e7bf1aed9175fada
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-25 15:01:43 +0200

    Update oci_gw.tf

--- oci_gw.tf
+++ oci_gw.tf
@@ -4,7 +4,7 @@ data "oci_core_services" "oci_services" {
 
 resource "oci_core_service_gateway" "oci_gw" {
   compartment_id = local.subnet.compartment_id
-  vcn_id = ocal.subnet.vcn_id
+  vcn_id = local.subnet.vcn_id
 
   display_name = local.name
 
